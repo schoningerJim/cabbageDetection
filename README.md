02c584addbc5a884606abf1969a27be886866ee0
schoningerJim <schoninger@enroute.co.jp>
2019-02-06 18:55:10 +0900

    added the first version of the dectection + readme + licence

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# cabbageDetection
-Building a Real-time cabbage-Detector using Neural Networks (SSD) on Tensorflow 
+# Real Time cabbage detection with Tensorflow SSD
+This project contains all the different files and scrips used to deploy a realtime
+cabbage detection algorithm using Tensorflow object detection API. As work task, 
+i was in charge to develop and deploy a realtime object detection for a drone.
+Basically, the first step and the most time consuming part was to gather the dataset.
+In order to do that, we manually took picture of different cabbage field with different point of view.
+If ypu wish to detect a more common object, you can refer to the opensource images database to check if your object is included. Here is the [link](https://storage.googleapis.com/openimages/web/index.html)
+From those pictures, we could manually label them by using LabelImg. It produces the xml in PASCALVOC format which is easier to use for the following part. To have more information about how to retrain the tensorflow object detection API, please refer to [this link](https://github.com/tensorflow/models/tree/master/research/object_detection).
+
+The goal of this repository is to show how deep learning can be applied in daily life and also to provide that can be adapted to some other uses case.
+
+## Table of contents
+
+1. [Installation](#instllation)
+2. [Utilisation](#utilisation)
+3. [Contributing](#contributing)
+4. [License](#licence)
+
+## Installation
+This model was trained using a NVIDIA GPU. First of all, please install all the NVIDIA requirements as explained in the official website. [Nvidia official page](https://developer.nvidia.com/).  
+Secondly please refer to the official [Tensorflow](https://www.tensorflow.org/install/install_linux#tensorflow_gpu_support) website to know the requirements for the tensorflow-gpu.
+>Note: I have exported my model with tensorflow 1.12.0.  Using another version may cause errors, so please export the model yourself with the model checkpoint to fit your tensorflow version. The export can be done by following the [step](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/exporting_models.md)here.  
+Moreover, I strongly recommend to use virtual environment to do this project else some errors might occur due to the libraries dependencies.  
+Virtual environment is good for python development because it can let you having many different working environments which depends on differents libraries without having conflicts.  
+If you do not know many about virtual env, [please refer to this excellent tutorial](https://realpython.com/python-virtual-environments-a-primer/)
+
+Please refer to the requirements.txt to install all the others dependencies. Be sure to be in the virtual environment before typing the following command.
+- `pip install -r requirements.txt`
+
+## Utilisation
+
+## Contributing
+
+## Licence
+Apache Licence. See [LICENSE](LICENSE) for details. Copyright (c) 2018 Enroute.
---
+++ detect_single_threaded.py
@@ -0,0 +1,120 @@
+from utils import detector_utils as detector_utils
+import cv2
+import tensorflow as tf
+import datetime
+import argparse
+
+detection_graph, sess = detector_utils.load_inference_graph()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-sth',
+        '--scorethreshold',
+        dest='score_thresh',
+        type=float,
+        default=0.2,
+        help='Score threshold for displaying bounding boxes')
+    parser.add_argument(
+        '-fps',
+        '--fps',
+        dest='fps',
+        type=int,
+        default=1,
+        help='Show FPS on detection/display visualization')
+    parser.add_argument(
+        '-src',
+        '--source',
+        dest='video_source',
+        default=0,
+        help='Device index of the camera.')
+    parser.add_argument(
+        '-wd',
+        '--width',
+        dest='width',
+        type=int,
+        default=320,
+        help='Width of the frames in the video stream.')
+    parser.add_argument(
+        '-ht',
+        '--height',
+        dest='height',
+        type=int,
+        default=180,
+        help='Height of the frames in the video stream.')
+    parser.add_argument(
+        '-ds',
+        '--display',
+        dest='display',
+        type=int,
+        default=1,
+        help='Display the detected images using OpenCV. This reduces FPS')
+    parser.add_argument(
+        '-num-w',
+        '--num-workers',
+        dest='num_workers',
+        type=int,
+        default=4,
+        help='Number of workers.')
+    parser.add_argument(
+        '-q-size',
+        '--queue-size',
+        dest='queue_size',
+        type=int,
+        default=5,
+        help='Size of the queue.')
+    args = parser.parse_args()
+
+    cap = cv2.VideoCapture(args.video_source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
+
+    start_time = datetime.datetime.now()
+    num_frames = 0
+    im_width, im_height = (cap.get(3), cap.get(4))
+    # max number of object we want to detect/track
+    num_max_detect = 4
+
+    cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
+
+    while True:
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        ret, frame = cap.read()
+        # image_np = cv2.flip(image_np, 1)
+        try:
+            image_np = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image_np = cv2.resize(image_np, (300, 300))
+        except:
+            print("Error converting to RGB")
+
+        # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+        # while scores contains the confidence for each of these boxes.
+        # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
+
+        boxes, scores = detector_utils.detect_objects(image_np,
+                                                      detection_graph, sess)
+
+        # draw bounding boxes on frame
+        detector_utils.draw_box_on_image(num_max_detect, args.score_thresh,
+                                         scores, boxes, im_width, im_height,
+                                         frame)
+
+        # Calculate Frames per second (FPS)
+        num_frames += 1
+        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+        fps = num_frames / elapsed_time
+
+        if (args.display > 0):
+            # Display FPS on frame
+            if (args.fps > 0):
+                detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
+                                                 frame)
+
+            cv2.imshow('Single-Threaded Detection', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+        else:
+            print("frames processed: ", num_frames, "elapsed time: ",
+                  elapsed_time, "fps: ", str(int(fps)))
